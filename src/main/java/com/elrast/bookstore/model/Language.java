3ea8f3c21e97805e5f78dd01785d94e3df198fcb
Iman <iman.khadiv@gmail.com>
2017-12-16 12:55:06 -0800

    added persitance.xml and Book entity

---
+++ src/main/java/com/elrast/bookstore/model/Language.java
@@ -0,0 +1,9 @@
+package com.elrast.bookstore.model;
+
+/**
+ * Created by Iman on 16/12/17.
+ */
+public enum Language {
+
+    ENGLISH, FRENCH, SPANISH, PORTUGUESE, ITALIAN, FINISH, GERMAN, DETACH, RUSSIAN
+}
