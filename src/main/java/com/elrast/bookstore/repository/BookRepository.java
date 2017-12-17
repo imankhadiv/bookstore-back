a592b1391745efd0faecdbb4b226aea7ed4dad68
Iman <iman.khadiv@gmail.com>
2017-12-16 19:45:41 -0800

    created book repository

---
+++ src/main/java/com/elrast/bookstore/repository/BookRepository.java
@@ -0,0 +1,45 @@
+package com.elrast.bookstore.repository;
+
+
+import com.elrast.bookstore.model.Book;
+
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+import javax.persistence.TypedQuery;
+import javax.transaction.Transactional;
+import java.util.List;
+
+import static javax.transaction.Transactional.TxType.REQUIRED;
+import static javax.transaction.Transactional.TxType.SUPPORTS;
+
+@Transactional(SUPPORTS)
+public class BookRepository {
+
+    @PersistenceContext(unitName = "bookStorePU")
+    private EntityManager entityManager;
+
+    public Book find(Long id) {
+        return entityManager.find(Book.class, id);
+    }
+
+    @Transactional(REQUIRED)
+    public Book create(Book book) {
+        entityManager.persist(book);
+        return book;
+    }
+    @Transactional(REQUIRED)
+    public void delete(Long id) {
+        entityManager.remove(entityManager.getReference(Book.class, id));
+    }
+
+    public List<Book> findAll() {
+        TypedQuery<Book> query = entityManager.createQuery("select b from Book b order by b.title DESC ", Book.class);
+        return query.getResultList();
+    }
+
+    public Long countAll() {
+        TypedQuery<Long> query = entityManager.createQuery("select count(b) from Book b ", Long.class);
+        return query.getSingleResult();
+    }
+
+}
